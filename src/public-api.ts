7c71c97e4077267362d6ab045a607c60e756b87d
Haris <haris.spahic@comtrade.com>
2020-12-09 14:07:34 +0100

    Remove files

--- src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ng-loading-spinner
- */
-
-export * from './lib/ng-loading-spinner.service';
-export * from './lib/ng-loading-spinner.interceptor';
-export * from './lib/ng-loading-spinner.component';
-export * from './lib/ng-loading-spinner.module';
